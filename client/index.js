61240a5fe7cb3480ae2df090c744c41b106b3f4a
albie-feary <laurabeth.feary@gmail.com>
2017-05-15 10:50:11 +1200

    add temp variables

--- client/index.js
+++ client/index.js
@@ -3,14 +3,15 @@ var ReactDOM = require('react-dom')
 
 function helloTemplate (props) {
   return ( <span>
-    <div>hello {props.name}</div>
+    <div>hello {props.person}</div>
     <div>Date { getDate() }</div>
+    {weather(props)}
     </span>
   )
 }
 function footerTemplate (props) {
   return (
-    <div>goodbye {props.name}</div>
+    <div>goodbye {props.person}</div>
   )
 }
 function getDate () {
@@ -20,13 +21,39 @@ function getDate () {
     <span> {currentDate.toDateString()} </span>
   )
 }
+function weather(props) {
+  return (
+    <div>
+      <span>
+        The temperature today is: {props.weather.temperature}
+      </span>
+      <span>
+        The conditons today are: {props.weather.condition}
+      </span>
+      <span>
+        We suggest you: {props.weather.suggestion}
+      </span>
+    </div>
+  )
+
+}
 
 
 
+var personObject = {
+    person: 'Sugar Cube',
+    weather: {
+      temperature: 15,
+      condition: 'raining',
+      suggestion: 'Stay inside, or you will melt!!!'
+    }
+  }
 
 var data = { name: 'mix' }
-var view = helloTemplate(data)
-var footerView = footerTemplate(data)
+
+var view = helloTemplate(personObject)
+
+var footerView = footerTemplate(personObject)
 
 var placeToMount = document.getElementById('root')
 var footer = document.getElementById('footer')
