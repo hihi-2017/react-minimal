bf9959b42d9ffc3e4f9710a3dda0799ad54cb728
matt-raines <mattraines.nz@gmail.com>
2017-05-15 10:47:02 +1200

    added date function

--- client/index.js
+++ client/index.js
@@ -1,27 +1,37 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-function helloTemplate (props) {
+function helloTemplate(props) {
+  console.log('props', props);
   return (
     <span>
-    <div>hello {props.name}</div>
+      <div>
+        hello {props.name}
+      </div>
+      <div>
+        todays date is: {props.day}/{props.month}/{props.year}
+      </div>
     </span>
   )
 }
 
 function renderFooter(props) {
-  return (
-    <h1>i am a footer</h1>
+  return ( <
+    h1 > i am a footer < /h1>
   )
 }
+
 function renderHeader(props) {
-  return (
-    <h1>i am a header</h1>
+  return ( <
+    h1 > i am a header < /h1>
   )
 }
 
 
-var data = { name: 'mix' }
+
+var data = {
+  name: 'mix, ', day: new Date().getDate(), month: new Date().getMonth(), year: new Date().getFullYear()
+}
 var view = helloTemplate(data)
 var footer = renderFooter()
 var header = renderHeader()
