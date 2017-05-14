fa45e8e145acadb0bd18fa963408cfe8cfb326e4
matt-raines <mattraines.nz@gmail.com>
2017-05-15 10:22:25 +1200

    Add footer and header

--- client/index.js
+++ client/index.js
@@ -3,14 +3,32 @@ var ReactDOM = require('react-dom')
 
 function helloTemplate (props) {
   return (
+    <span>
     <div>hello {props.name}</div>
+    </span>
   )
 }
 
+function renderFooter(props) {
+  return (
+    <h1>i am a footer</h1>
+  )
+}
+function renderHeader(props) {
+  return (
+    <h1>i am a header</h1>
+  )
+}
+
+
 var data = { name: 'mix' }
 var view = helloTemplate(data)
-
+var footer = renderFooter()
+var header = renderHeader()
 var placeToMount = document.getElementById('root')
+var footerMountPoint = document.getElementById('footer')
+var headerMountPoint = document.getElementById('header')
 
 ReactDOM.render(view, placeToMount)
-
+ReactDOM.render(footer, footerMountPoint)
+ReactDOM.render(header, headerMountPoint)
