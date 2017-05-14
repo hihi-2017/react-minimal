274b09a5bae39566942f30bb9be06c22ab3533f1
kelly-keating <keating.kel@gmail.com>
2017-05-15 11:32:18 +1200

    condensed it into one render

--- client/index.js
+++ client/index.js
@@ -42,33 +42,47 @@ function showTask(tasks) {
   )
 }
 
-var data = { name: 'cici' }
-var sugarCube = {
+function showAll (data) {
+    return(
+      <div>
+        <div>{helloTemplate(data.sugarCube)}</div>
+        <div>{weather(data.sugarCube)}</div>
+        <div>{showTask(data.taskData)}</div>
+        <div>{goodbyeTemplate(data)}</div>
+      </div>
+    )
+}
+
+var data = {
+  name: 'cici',
+  sugarCube: {
     person: 'Sugar Cube',
     weather: {
       temperature: 15,
       condition: 'raining',
       suggestion: 'Stay inside, or you will melt!!!'
     }
-  }
-var taskData = [
-  {description:'get curry at Great India', status:'not done'},
-  {description:'eat lunch', status:'not done'},
-  {description:'eat dinner', status:'not done'},
-  {description:'take a walk on the waterfront', status:'done'}
-]
+  },
+  taskData : [
+    {description:'get curry at Great India', status:'not done'},
+    {description:'eat lunch', status:'not done'},
+    {description:'eat dinner', status:'not done'},
+    {description:'take a walk on the waterfront', status:'done'}
+  ]
+}
 
-var view = helloTemplate(sugarCube)
-var weatherSuggest = weather(sugarCube)
-var endView = goodbyeTemplate(data)
-var viewTask = showTask(taskData)
+var view = showAll(data)
+// var view = helloTemplate(sugarCube)
+// var weatherSuggest = weather(sugarCube)
+// var endView = goodbyeTemplate(data)
+// var viewTask = showTask(taskData)
 
 var header = document.getElementById('root')
-var weather = document.getElementById('weather')
-var footer = document.getElementById('footer')
-var tasks = document.getElementById('tasks')
+// var weather = document.getElementById('weather')
+// var footer = document.getElementById('footer')
+// var tasks = document.getElementById('tasks')
 
 ReactDOM.render(view, header)
-ReactDOM.render(weatherSuggest, weather)
-ReactDOM.render(endView, footer)
-ReactDOM.render(viewTask,tasks)
+// ReactDOM.render(weatherSuggest, weather)
+// ReactDOM.render(endView, footer)
+// ReactDOM.render(viewTask,tasks)
