28d1a0adf06424d587cf1c367c6fd1f8f99705e2
todddrinkwater <daniel-reason>
2017-05-15 11:05:19 +1200

    We found salt cube with ternary operator

--- client/index.js
+++ client/index.js
@@ -3,9 +3,18 @@ var ReactDOM = require('react-dom')
 
 function helloTemplate (props) {
   return (
-    <div>hello {props.person} the temperature is {props.weather.temperature} degrees and it is {props.weather.condition} outside, we suggest {props.weather.suggestion}</div>
+    <div>
+      hello {props.person} the temperature is {props.weather.temperature} degrees and it is {props.weather.condition} outside, we suggest {props.weather.suggestion}
+    </div>
   )
 }
+
+function whoIsThere(props) {
+  (props.person == "Sugar Cube") ?
+  return helloTemplate(props)
+  : return <div> Where is Sugar Cube</div>
+}
+
 
 function helloDoctor (props) {
   return (
@@ -17,7 +26,7 @@ var date = {date: 'Monday, May 15th, 2017'}
 var data = { name: 'Alan' }
 var footData = {title: 'Dr. Foot', occupation: "Podiatrist"}
 var sugarWeather =   {
-    person: 'Sugar Cube',
+    person: 'Salt Cube',
     weather: {
       temperature: 15,
       condition: 'raining',
@@ -27,9 +36,10 @@ var sugarWeather =   {
 
 var view = helloTemplate(sugarWeather)
 var foot = helloDoctor(footData)
+var who = whoIsThere(sugarWeather)
 
 var placeToMount = document.getElementById('root')
 var footerMount = document.getElementById('footer')
 
-ReactDOM.render(view, placeToMount)
+ReactDOM.render(who, placeToMount)
 ReactDOM.render(foot, footerMount)
