7647d0da9083a51d985e2bc5940d33440711b388
nat-andrew <andy@andycondon.com>
2017-05-15 11:46:10 +1200

    hello, goodbye and date added

--- client/index.js
+++ client/index.js
@@ -2,15 +2,28 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 
 function helloTemplate (props) {
+  console.log(props.today)
   return (
-    <div>hello {props.name}</div>
+    <div>hello {props.name}. The date today is: {props.today.toString()}.</div>
   )
 }
 
-var data = { name: 'mix' }
+function goodbyeTemplate(info) {
+  return (
+    <div>goodbye {info.name}</div>
+  )
+}
+
+
+var data = { name: 'hihi',
+            today: new Date()}
 var view = helloTemplate(data)
 
+let info = { name: 'Pork Pie'}
+var footerView = goodbyeTemplate(info)
+
 var placeToMount = document.getElementById('root')
+var footerMount = document.getElementById('footer')
 
 ReactDOM.render(view, placeToMount)
-
+ReactDOM.render(footerView, footerMount)
