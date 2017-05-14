f7e7a4026a66fb30fb6a515a1813e8af847a4c94
nat-andrew <andy@andycondon.com>
2017-05-15 10:38:50 +1200

    warn the sugar cube about the rain

--- client/index.js
+++ client/index.js
@@ -2,15 +2,23 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 
 function helloTemplate (props) {
+  console.log(props)
   return (
-    <div>hello {props.name}</div>
+    <div>hello {props.person}. Weather today is {props.weather.condition} with a high of {props.weather.temperature}&deg;. {props.weather.suggestion} </div>
   )
 }
 
-var data = { name: 'mix' }
+var data = {
+    person: 'Sugar Cube',
+    weather: {
+      temperature: 15,
+      condition: 'raining',
+      suggestion: 'Stay inside, or you will melt!!!'
+    }
+  }
 var view = helloTemplate(data)
 
 var placeToMount = document.getElementById('root')
+var footerMount = document.getElementById('footer')
 
 ReactDOM.render(view, placeToMount)
-
