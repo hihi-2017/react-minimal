4de641fe738ea59a7a8030cd395cd5b8dc224a61
bill-burgess <wmhburgess@gmail.com>
2017-05-15 10:39:11 +1200

    Experiment with react

--- client/index.js
+++ client/index.js
@@ -1,16 +1,55 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-function helloTemplate (props) {
+function helloTemplate(props) {
   return (
-    <div>hello {props.name}</div>
+    <div>
+      {
+        props.person.map((thing) => <div>hello {thing} it is {props.weather.condition}, I suggest you {props.weather.suggestion} because your favourite number is {Math.random() * 100}</div>)
+      }
+    </div>
   )
 }
 
-var data = { name: 'mix' }
-var view = helloTemplate(data)
+function footerStuff(props) {
+  return (
+    <div>This is the footer, {props.name}</div>
+  )
+}
+
+var data = {
+  name: 'mix'
+}
+
+var cube = {
+  person: [
+    'Sugar Cube', 'Cherry Pie'
+  ],
+  weather: {
+    temperature: 15,
+    condition: 'raining',
+    suggestion: 'Stay inside, or you will melt!!!'
+  }
+}
+var view = helloTemplate(cube)
 
 var placeToMount = document.getElementById('root')
+var footerStuff = footerStuff(data)
+var destination = document.getElementById('footer')
 
-ReactDOM.render(view, placeToMount)
+function footer() {
+  ReactDOM.render(footerStuff, destination)
+}
 
+function pageContent() {
+  ReactDOM.render(view, destination)
+
+}
+
+function randomPage() {
+  (Math.random() > 0.5)
+    ? footer()
+    : pageContent()
+}
+
+randomPage()
